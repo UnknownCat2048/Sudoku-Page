34e75fe4fe8d31f60e7f2ee8e73061306bf27cb9
UnknownCat2048 <unknowncat2048@gmail.com>
2025-01-03 14:38:12 +0800

    上传 v2.0.0

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "UnknownCat2048-Sudoku-2.0.0";
+const contentToCache = [
+    "Build/Sudoku-v2.0.0-WebGL.loader.js",
+    "Build/Sudoku-v2.0.0-WebGL.framework.js.unityweb",
+    "Build/Sudoku-v2.0.0-WebGL.data.unityweb",
+    "Build/Sudoku-v2.0.0-WebGL.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
